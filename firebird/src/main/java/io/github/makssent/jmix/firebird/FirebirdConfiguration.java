128e9c33055d296fd6dbea774166a12615161955
makssent <igoshinreceived@yandex.ru>
2026-07-29 11:31:14 +0300

    Remove Flow UI scaffolding from the add-on

--- firebird/src/main/java/io/github/makssent/jmix/firebird/FirebirdConfiguration.java
+++ firebird/src/main/java/io/github/makssent/jmix/firebird/FirebirdConfiguration.java
@@ -1,42 +1,16 @@
 package io.github.makssent.jmix.firebird;
 
 import io.jmix.core.annotation.JmixModule;
-import io.jmix.core.impl.scanning.AnnotationScanMetadataReaderFactory;
 import io.jmix.eclipselink.EclipselinkConfiguration;
-import io.jmix.flowui.FlowuiConfiguration;
-import io.jmix.flowui.sys.ActionsConfiguration;
-import io.jmix.flowui.sys.ViewControllersConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.Collections;
-
 @Configuration
 @ComponentScan
 @ConfigurationPropertiesScan
-@JmixModule(dependsOn = {EclipselinkConfiguration.class, FlowuiConfiguration.class})
+@JmixModule(dependsOn = EclipselinkConfiguration.class)
 @PropertySource(name = "io.github.makssent.jmix.firebird", value = "classpath:/io/github/makssent/jmix/firebird/module.properties")
 public class FirebirdConfiguration {
-
-    @Bean("firebird_FirebirdViewControllers")
-    public ViewControllersConfiguration screens(final ApplicationContext applicationContext,
-                                                final AnnotationScanMetadataReaderFactory metadataReaderFactory) {
-        final ViewControllersConfiguration viewControllers
-                = new ViewControllersConfiguration(applicationContext, metadataReaderFactory);
-        viewControllers.setBasePackages(Collections.singletonList("io.github.makssent.jmix.firebird"));
-        return viewControllers;
-    }
-
-    @Bean("firebird_FirebirdActions")
-    public ActionsConfiguration actions(final ApplicationContext applicationContext,
-                                        final AnnotationScanMetadataReaderFactory metadataReaderFactory) {
-        final ActionsConfiguration actions
-                = new ActionsConfiguration(applicationContext, metadataReaderFactory);
-        actions.setBasePackages(Collections.singletonList("io.github.makssent.jmix.firebird"));
-        return actions;
-    }
 }
